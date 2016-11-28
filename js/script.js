33837fddf6f0652de53a8c7494c3ed75db9e981c
jakub <mroczkowski.jakub84@gmail.com>
2016-11-28 16:14:36 +0100

    Instrukcje warunkowe, sprawdzenie czy 0

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var a = prompt('Podaj pierwszą wartość'),
+	b = prompt('Podaj drugą wartość'),
+	value = (a*a) + (2 * a * b) - (b * b);
+
+console.log(value);
+document.write('Podałeś pierwszą wartość: ' + a);
+document.write('<br >');
+document.write('Podałeś drugą wartość: ' + b);
+document.write('<br >');
+
+if (value > 0) {
+	console.log('Wartość value jest większa od 0');
+	document.write('Wynik działania (a*a) + (2 * a * b) - (b * b) jest większy od zera i wynosi: ' + value);
+} else if(value == 0){
+	console.log('Wartość value jest równa 0');
+	document.write('Wynik działania (a*a) + (2 * a * b) - (b * b) wynosi: ' + value);
+} 
+else  {
+	console.log('Wartość value jest mniejsza od 0');
+	document.write('Wynik działania (a*a) + (2 * a * b) - (b * b) jest mniejszy od zera i wynosi: ' + value);
+}
